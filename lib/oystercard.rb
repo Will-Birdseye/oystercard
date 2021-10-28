785e0edc561ca9e280b85b81336ba7428816c3a4
Will <w_birdseye@hotmail.co.uk>
2021-10-28 11:55:00 +0100

    Challenge 7 complete deduct method added and tested

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -12,5 +12,9 @@ class Oystercard
     fail 'Maximum balance of #{maximum_balance} exceeded' if amount + balance > MAXIMUM_BALANCE
     @balance += amount
   end
+
+  def deduct(amount)
+    @balance -= amount
+  end
   
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,8 +7,6 @@ describe Oystercard do
 
   describe '#top_up' do
 
-    it { is_expected.to respond_to(:top_up).with(1).argument }
-
     it 'can top up the balance' do
       expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
     end
@@ -18,5 +16,10 @@ describe Oystercard do
       subject.top_up(maximum_balance)
       expect{ subject.top_up 1 }.to raise_error 'Maximum balance of #{maximum_balance} exceeded'
     end
+  end
+
+  it 'deducts an amount from the balance' do
+    subject.top_up(20)
+    expect{ subject.deduct 3}.to change{ subject.balance }.by -3
   end
 end
